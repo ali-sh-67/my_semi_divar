868a6ee7636a43221e4390a66306f856d7356aa2
ali-sh-67 <ali.shahbeaki@gmail.com>
2021-11-29 09:09:16 +0330

    create and modify users migrations

--- divar-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ divar-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id()->primary();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer("phone_number",);
+            $table->string('country' );
+            $table->string("city");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
