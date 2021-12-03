4d2de452deb46fcbf35c891b3ceecac79376d3cf
ali-sh-67 <ali.shahbeaki@gmail.com>
2021-12-03 23:25:47 +0330

    elementry index create store controller for category

--- divar-app/database/migrations/2021_11_30_113336_create-categories_table.php
+++ divar-app/database/migrations/2021_11_30_113336_create-categories_table.php
@@ -15,9 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('name_en');
-            $table->bigInteger('parent_id');
+            $table->bigInteger('parent_id')->nullable();
             $table->timestamps();
         });
     }
