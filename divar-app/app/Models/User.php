7db9259d03036c339379dc7065f14c503b6a8d7a
ali-sh-67 <ali.shahbeaki@gmail.com>
2021-12-04 01:00:09 +0330

    edit on model user

--- divar-app/app/Models/User.php
+++ divar-app/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'id',
-        'first_name',
+        'name',
         'last_name',
         'email',
         'email_verified_at',
