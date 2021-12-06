7a530fe60e72287445659f28c1c18275036f5ba0
JavadMohammadi <mehrab548@yahoo.com>
2021-12-06 16:49:48 +0330

    createAdsController

--- divar-app/resources/views/layouts/main.blade.php
+++ divar-app/resources/views/layouts/main.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>main</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<body>
+<body dir="rtl" style="text-align:right">
+
 @yield('content')
 </body>
 </html>
