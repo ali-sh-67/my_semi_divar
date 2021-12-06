b09e5016a6d70f34a92bfb2b3bf6ed973f8a04c4
JavadMohammadi <mehrab548@yahoo.com>
2021-12-06 16:23:54 +0330

    create AdsController

--- divar-app/routes/web.php
+++ divar-app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\categoryController;
+use App\Http\Controllers\AdsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Ad/createAd', [AdsController::class, 'createAd'])->name('createAd');
+Route::post('/Ad/storeAd', [AdsController::class, 'storeAd'])->name('storeAd');
+Route::get('/Ad/index', [AdsController::class, 'indexAd'])->name('indexAd');
+Route::get('/Ad/showAd/{id}', [AdsController::class, 'showAd'])->name('showAd');
+Route::get('/Ad/deleteAd/{id}',[AdsController::class, 'deleteAd'])->name('deleteAd')->middleware('auth');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
