de3b1a351ff9f49ca2eae67a20391fb457959fed
JavadMohammadi <mehrab548@yahoo.com>
2021-12-06 16:23:16 +0330

    create rout for Ads

---
+++ divar-app/resources/views/Ad/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+@section('content')
+
+<div class="container">
+
+<div class="d-flex justify-content-center">
+<table class="table">
+    
+    <thead>    
+    <tr>           
+        <td>عنوان آگهی</td>                 
+        <td>ویرایش</td>
+        <td>حذف</td>
+        <td>مشاهده</td>
+    </tr>
+    </thead>
+    <tbody>
+   @foreach($ads as $ad)
+  
+    <tr>           
+        <td>{{$ad->title}}</td>                  
+        <td><a href="">ویرایش</a></td>
+        <td><a href="{{route('deleteAd',$ad->id)}}"" onclick="return confirm('آیتم مورد نظر حذف شود؟');">حذف</a></td>
+        <td><a href="{{route('showAd',$ad->id)}}">مشاهده</a></td> 
+    </tr>    
+    
+    @endforeach
+        
+    </tbody>
+</table>
+</div>
+</div>
+
+
+
+@endsection
