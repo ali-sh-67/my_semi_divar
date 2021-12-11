03c462509360935c0f764971bed4928bb4bc59b5
JavadMohammadi <mehrab548@yahoo.com>
2021-12-12 00:00:55 +0330

    add file

--- divar-app/app/Http/Controllers/AdsController.php
+++ divar-app/app/Http/Controllers/AdsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\ad;
 use App\Models\category;
-
+use App\Models\comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,8 +13,8 @@ class AdsController extends Controller
     {  
         $users = Auth::user()->name;            
         $id = Auth::user()->id;       
-         $ads = Ad::where('user_id',$id)->get();
-        return view('Ad.index')->with(['ads' => $ads]);
+        $ads = Ad::where('user_id',$id)->orderBy('id','Desc')->paginate(5);
+        return view('Ad.pageAd')->with(['ads' => $ads]);
     }
 
    public function createAd()
@@ -24,18 +24,22 @@ class AdsController extends Controller
 
    public function storeAd(Request $request)
     {
+        $newImageName =time() . '-' . $request->name . '.' . $request->image_url->extension();
+        // $request->image_url->extension();
+        $request->image_url->move(public_path('images'),$newImageName);
+
         $user = Auth::user()->id;
         $category = Auth::user()->id;
         
         $ad=new Ad([
-        'user_id'=>$user,
-        'category_id'=>$category,
-        'title'=> $request->get('title'),
-        'description'=>$request->get('description'),
-        'image_url'=>$request->get('image_url'),
-        'price'=>$request->get('price'),
-        'address'=>$request->get('address'),
-        'phone_number_ads'=> $request->get('phone_number_ads'),
+            'user_id'=>$user,
+            'category_id'=>$category,
+            'title'=> $request->get('title'),
+            'description'=>$request->get('description'),
+            'image_url'=>$newImageName,
+            'price'=>$request->get('price'),
+            'address'=>$request->get('address'),
+            'phone_number_ads'=> $request->get('phone_number_ads'),
         ]);
         
         
@@ -49,18 +53,26 @@ class AdsController extends Controller
 
     public function deleteAd(Ad $id)   
     {    
-    $id->delete();
-    return redirect(route('indexAd'));
-    }
-    
+        $id->delete();
+        return redirect(route('indexAd'));
+    }    
     public function showAd(Ad $id)
-    {  
-        
-        // dd($id);
+    {         
         $users = Auth::user()->name;             
         return view('Ad.showAd')->with(['id'=>$id]);   
     }
-
-    
-    
+    public function editAd(Ad $id)
+    {   
+        $users = Auth::user()->name;  
+        return view('Ad.editAd')->with(['id'=>$id])->with(['users'=>$users]);            
+    }
+    public function updateAd(Request $request, $id)
+    {
+        $todo = Ad::where('id', $id)->first();           
+        $todo->title= $request->get('title');
+        $todo->description= $request->get('description');
+        $todo->price= $request->get('price');         
+        $todo->save();                
+        return redirect(route('indexAd'));   
+    }    
 }
