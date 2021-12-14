f0b354cdce2bc3cd5e72c9c71b16f191b9f860f1
JavadMohammadi <mehrab548@yahoo.com>
2021-12-14 19:45:07 +0330

    change this controller

--- divar-app/app/Http/Controllers/commentController.php
+++ divar-app/app/Http/Controllers/commentController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\ad;
 use App\Models\category;
@@ -13,23 +12,25 @@ class commentController extends Controller
     public function createComment(Ad $id){
         // dd($id);
         return view('Comment.create')->with(['id'=>$id]);
+
     }
 
 
-    public function StoreComment(Request $request, $id){        
-        
-            
-      
-        $comment=new comment([
-            'Ads_id'=>$id ,                    
-            'description'=>$request->get('description'),           
-            ]);
-            
+    public function StoreComment(Request $request, $id){         
 
-            if ($comment->save()) {
-             
-                return redirect(route('indexAd'));          
-            }    
+        $user_id = Auth::user()->id;      
+        $Ads_id  = ad::where('id',$id)->first()->id;         
+
+        $comment=new comment([            
+            'user_id'=> $user_id,                 
+            'Ads_id'=> $Ads_id,                 
+            'description'=> $request->get('description'),           
+            ]);       
+
+        if($comment->save())
+        {              
+            return redirect(route('indexAd'));          
+        }    
 
     }
 }
