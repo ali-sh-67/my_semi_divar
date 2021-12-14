e45bc8247249e324380f2d1b05d730f740030d67
JavadMohammadi <mehrab548@yahoo.com>
2021-12-14 19:46:40 +0330

    change this model

--- divar-app/app/Models/comment.php
+++ divar-app/app/Models/comment.php
@@ -10,9 +10,20 @@ class comment extends Model
     use HasFactory;
     protected $fillable = [
         'id',
+        'user_id',
         'Ads_id', 
         'description',       
         'created_at',
         'updated_at',
     ];
+    public function Ad()
+    {
+        return $this->belongsTo(Ad::class);
+    }
+    public function User()
+    {
+
+        return $this->belongsTo(User::class, 'user_id','id');
+    }
+    
 }
